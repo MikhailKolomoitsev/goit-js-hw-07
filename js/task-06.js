5fb40f672f814f49c1133f8b3a341ccbab1f782d
mihon.testosterone <mihon.testosterone@gmail.com>
2021-05-21 12:08:21 +0300

    task 6

---
+++ js/task-06.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('#validation-input')
+input.addEventListener('input', onBorderColor)
+
+
+function onBorderColor() {
+const checkedValue = input.value.split('').length
+    console.log(checkedValue);
+
+     if (checkedValue == 0) {
+        input.classList.remove('invalid')
+        input.classList.remove('valid')
+    }
+    else if (checkedValue < input.getAttribute('data-length'))
+    {
+        input.classList.add('invalid')
+        input.classList.remove('valid')}
+ 
+    else if (checkedValue > input.getAttribute('data-length')) {
+        input.classList.replace('invalid', 'valid');
+    }
+
+}
